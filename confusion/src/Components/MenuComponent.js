988cbd88f801e49653ff4798b36b8675c3b84e03
brandon velasquez <brandon.velasquez.osorio@gmail.com>
2021-01-01 12:05:44 -0500

    Implementacion de componentes funcionales

--- confusion/src/Components/MenuComponent.js
+++ confusion/src/Components/MenuComponent.js
@@ -1,31 +1,32 @@
-import React, { Component } from 'react';
+import React from 'react';
 import "./MenuComponent.css";
 import { Card, CardImg, CardImgOverlay, CardTitle} from 'reactstrap';
 
-class Menu extends Component {
-    constructor(props) {
-        super(props);
-    }
-
-    render() {
-        const menu= this.props.platos.map((plato) => {
-            return(<div key={plato.id} className="col-12 col-md-5 m-1">
-                <Card onClick={() => this.props.onClick(plato.id)}>
-                    <CardImg width="100%" src = {plato.image} alt= {plato.name}/>
-                    <CardImgOverlay>
-                        <CardTitle className="titulo1">{plato.name}</CardTitle>
-                    </CardImgOverlay>
-                </Card>
-            </div>);
-        });
-        return (
-            <div className="container" >
-                <div className="row" >
-                    {menu}
-                </div >
-            </div>
-        );
-    }
+function RenderPlato({plato, onClick}){
+    return(
+        <Card onClick={() => onClick(plato.id)}>
+            <CardImg width="100%" src = {plato.image} alt= {plato.name}/>
+            <CardImgOverlay>
+                <CardTitle className="titulo1">{plato.name}</CardTitle>
+            </CardImgOverlay>
+        </Card>
+    );
 }
+
+const Menu = (props) => {
+    const menu= props.platos.map((plato) => {
+        return(<div key={plato.id} className="col-12 col-md-5 m-1">
+            <RenderPlato plato={plato} onClick={props.onClick(plato.id)}/> 
+        </div>);
+    });
+    return (
+        <div className="container" >
+            <div className="row" >
+                {menu}
+            </div >
+        </div>
+    );
+}
+        
 
 export default Menu;
--- confusion/src/Components/PlatoSeleccionadoComponent.js
+++ confusion/src/Components/PlatoSeleccionadoComponent.js
@@ -1,45 +1,56 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Card, CardImg, CardText, CardBody, CardTitle} from 'reactstrap';
-class PlatoSeleccionado extends Component {
 
-    render(){
-        let comentarios = null;
+function RenderPlato({plato}){
+    return(
+        <Card>
+            <CardImg width="100%" src = {plato.image} alt= {plato.name}/>
+            <CardBody>
+                <CardTitle className="titulo1">{plato.name}</CardTitle>
+                <CardText>{plato.description}</CardText>
+            </CardBody>
+        </Card>
+    );
+}
+
+ function RenderComentario({plato}){
+    const comentarios = plato.comments.map((comentario) => {
+        return(
+        <div key = {comentario.id}>
+            <h6>{comentario.author}:</h6>
+            
+            <p>{comentario.comment}</p>
+            <p><i><b>Rating:</b> {comentario.rating}</i></p>
+            <s>{new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit'}).format(new Date(Date.parse(comentario.date)))}</s>
+            <hr></hr>
+        </div>
+        );
+    });
+    return(
+        <div className="col-12 col-md-5 m-1">
+            <h4>COMENTARIOS</h4>
+            {comentarios}
+        </div>
+    );
+}
+
+
+    const PlatoSeleccionado = (props) => {
         let plato = null;
-        if(this.props.PlatoSeleccionado != null && this.props.PlatoSeleccionado !== undefined){
-            plato = this.props.PlatoSeleccionado;
-            comentarios = plato.comments.map((comentario) => {
-                return(
-                <div key = {comentario.id}>
-                    <h6>{comentario.author}:</h6>
-                    
-                    <p>{comentario.comment}</p>
-                    <p><i><b>Rating:</b> {comentario.rating}</i></p>
-                    <s>{new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit'}).format(new Date(Date.parse(comentario.date)))}</s>
-                    <hr></hr>
-                </div>
-                );
-            });
+        if(props.PlatoSeleccionado != null || props.PlatoSeleccionado !== undefined){
+            plato = props.PlatoSeleccionado;
+            
             return(
                 <div className="row">
                     <div className="col-12 col-md-5 m-1">
-                            <Card>
-                                <CardImg width="100%" src = {plato.image} alt= {plato.name}/>
-                                <CardBody>
-                                    <CardTitle className="titulo1">{plato.name}</CardTitle>
-                                    <CardText>{plato.description}</CardText>
-                                </CardBody>
-                            </Card>
+                        <RenderPlato plato={plato}/>
                     </div>
-                    <div className="col-12 col-md-5 m-1">
-                        <h4>COMENTARIOS</h4>
-                        {comentarios}
-                    </div>
+                    <RenderComentario plato={plato}/>
                 </div>
                 );
         }else{
             return(<div></div>);
         }
-
     }
-}
-export default PlatoSeleccionado;
+
+    export default PlatoSeleccionado;
