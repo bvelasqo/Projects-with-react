e36671a021dc34ead40b3baecb0257d93f038f82
brandon velasquez <brandon.velasquez.osorio@gmail.com>
2021-01-18 21:06:06 -0500

    React Redux Form Validation

--- confusion/src/Components/MainComponent.js
+++ confusion/src/Components/MainComponent.js
@@ -19,13 +19,6 @@ const mapStateToProps = state => {
 }
 
 class Main extends Component{
-    constructor(props) {
-      super(props);
-    }
-    
-
-    
-
     render(){
       const DishWithId = ({match}) => {
         return(
